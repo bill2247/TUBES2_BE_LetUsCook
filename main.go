0cc5fd672d0c2151724230a4eac08e48abd0834a
Diyah Susan <diyahnugrahani04@gmail.com>
2025-05-12 11:06:53 +0700

    feat : coba deploy

--- main.go
+++ main.go
@@ -28,7 +28,7 @@ func main() {
 	r := gin.Default()
 
 	r.Use(cors.New(cors.Config{
-		AllowOrigins:     []string{"http://localhost:3000"},
+		AllowOrigins:     []string{"https://let-us-cook-new.vercel.app"},
 		AllowMethods:     []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
 		AllowHeaders:     []string{"Origin", "Content-Type", "Accept", "Authorization"},
 		ExposeHeaders:    []string{"Content-Length"},
